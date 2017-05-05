a2b202476e621096702e54fb50b64220a76a66cb
Andrew Stout <andrew@andrewstout.me>
2017-05-05 16:40:21 -0400

    Made slight modifications to calculating perfect number

--- ecmascript/perfect-numbers/perfect-numbers.js
+++ ecmascript/perfect-numbers/perfect-numbers.js
@@ -4,11 +4,10 @@ class PerfectNumbers {
             throw new Error('Classification is only possible for natural numbers.');
         }
 
+        const limit = Math.floor(num / 2);
         let sum = 0;
-        for (let x = 1; x < num; x += 1) {
-            if (num % x === 0) {
-                sum += x;
-            }
+        for (let x = 1; x <= limit; x += 1) {
+            sum += num % x === 0 ? x : 0;
         }
 
         if (num === sum) {
